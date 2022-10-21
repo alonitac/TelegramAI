b71e3662c3b13b195c5776ea49c1494e042cef07
Cy-Yuval <einatman4@walla.com>
2022-10-21 13:54:30 +0300

    Added YoutubeBot Class, impr oved download_user_photo method and implemented videos cache mechanism

--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 import time
 from yt_dlp import YoutubeDL
-
+import os 
 
 def search_download_youtube_video(video_name, num_results=1):
     """
@@ -11,14 +11,25 @@ def search_download_youtube_video(video_name, num_results=1):
     """
     results = []
     with YoutubeDL() as ydl:
-        videos = ydl.extract_info(f"ytsearch{num_results}:{video_name}", download=True)['entries']
+        videos = ydl.extract_info(f"ytsearch{num_results}:{video_name}", download=False)['entries']
+        
+        file_exists = False
 
         for video in videos:
+            for file in os.listdir():
+                if file == ydl.prepare_filename(video):
+                    file_exists = True
+                    break
+
             results.append({
                 'filename': ydl.prepare_filename(video),
                 'video_id': video['id'],
                 'title': video['title'],
                 'url': video['webpage_url']
             })
+        
+        if file_exists == False:
+            videos = ydl.extract_info(f"ytsearch{num_results}:{video_name}", download=True)['entries']
+
 
     return results
