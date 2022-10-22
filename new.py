1645e7769001c5ca800ef0dc1fa6335385036315
polonskyilya <ilya@polonsky.net>
2022-10-22 16:33:21 +0300

    new

--- new.py
+++ new.py
@@ -2,26 +2,30 @@ from pythonping import ping
 import sys
 import os
 import mysql.connector
-'''dbname=os.environ.get('DB_NAME')
-mydb = mysql.connector.connect(host=os.environ.get('MYSQL_IP'),user=os.environ.get('MYSQL_ROOT_USER'),
-          password=os.environ.get('MYSQL_ROOT_PASSWORD'))
-mycursor = mydb.cursor()
-query = f"SELECT link_download FROM {dbname} WHERE search_phrase = 'strangeshithappened'"
-mycursor.execute(query)'''
+from mysql.connector import errorcode
 
-mydb=mysql.connector.connect(
+try:
+  cnx = mysql.connector.connect(
   host="192.168.20.200",
   user="ilya",
   password="ilya",
   auth_plugin='mysql_native_password'
 )
+except mysql.connector.Error as err:
+  if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+    m=f"Something is wrong with your user name or password {err.errno}"
+  elif err.errno == errorcode.ER_BAD_DB_ERROR:
+    m=f"Database does not exist {err.errno}"
+  else:
+    print(err)
+else:
+  cnx.close()
 
-mycursor = mydb.cursor()
-
-mycursor.execute("CREATE DATABASE mydatabase")
 with open('/img/file.txt', 'w') as sys.stdout:
     print(f'FROM ENV\n{os.environ.get("MYSQL_IP")}\n{os.environ.get("MYSQL_ROOT_USER")}\n'
           f'{os.environ.get("MYSQL_ROOT_PASSWORD")}\n{os.environ.get("API")}\n')
     print(f'GOOGLE PING\n{ping("8.8.8.8")}')
     print(f'SQL PING\n{ping("192.168.20.200")}')
+    print(m)
+    print(err)
 #    print(f'FROM SQL\n{mycursor.fetchone()}')
