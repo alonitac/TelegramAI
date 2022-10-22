300bac1f8d01cc8d9e5c58163b5bcf55c0719cc3
polonskyilya <ilya@polonsky.net>
2022-10-22 10:23:12 +0300

    new

--- new.py
+++ new.py
@@ -1,3 +1,5 @@
 from pythonping import ping
-print(f'GOOGLE PING\n{ping("8.8.8.8")}')
-print(f'SQL PING\n{ping("192.168.20.200")}')
+import sys
+with open('/img/file.txt', 'w') as sys.stdout:
+    print(f'GOOGLE PING\n{ping("8.8.8.8")}')
+    print(f'SQL PING\n{ping("192.168.20.200")}')
