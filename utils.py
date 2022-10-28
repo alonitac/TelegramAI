cdb56ad26807af03186d15fc3c0357164c9184fd
polonskyilya <ilya@polonsky.net>
2022-10-28 15:50:24 +0300

    app.py Documentations PEP 8 Corrections utils.py Documentation PEP 8 Corrections

--- utils.py
+++ utils.py
@@ -2,10 +2,21 @@ from yt_dlp import YoutubeDL
 from loguru import logger
 
 
-def search_download_youtube_video(video_name, num_results=1,user_id=0,download=False):
+def search_download_youtube_video(video_name, num_results=1, download=False):
+    '''
+    Function that fetches the urls of videos from youtube
+    :param video_name: Search text from user
+    :param num_results: number of results to show, default 1
+    :param download: boolean download --> if download as a file or not
+    :return: dictionary
+    '''
     with YoutubeDL() as ydl:
         logger.info(f"{__name__}YoutubeDL from Util initiated")
-        videos = ydl.extract_info(f"ytsearch{1}:{video_name}", download=False)['entries']
+        videos = ydl.extract_info(f"ytsearch{num_results}:{video_name}", download=download)['entries']
         for format in videos[0]['formats']:
             if format['format_id'] == '22' or format['format_id'] == '18':
-                return {'download_url':format["url"],'youtube_url':videos[0]['webpage_url']}
+                '''   
+                url --> url that allows to download a file on any
+                device (auto download in pc) save to galery in android
+                '''
+                return {'download_url': format["url"], 'youtube_url': videos[0]['webpage_url']}
