f51ec41cbddeee31b13103ddeee08b45853a8e11
polonskyilya <ilya@polonsky.net>
2022-10-28 13:38:23 +0300

    changes in app.py for container changes in Docker-compose.yaml changes in requirements.txt changes in utils.py

--- app.py
+++ app.py
@@ -1,50 +1,144 @@
-import telebot,sqlite3,utils,re
-from utils import search_download_youtube_video
+import telebot,sqlite3,utils,os,uuid,datetime
 from loguru import logger
-from os import environ
-
-#dbconnect = sqlite3.connect(r'c:\Users\Ilya Polonsky\Desktop\users9.db3')
-#pointer = dbconnect.cursor()
-#pointer.execute('CREATE TABLE IF NOT EXISTS user_bot (USER_BOT_ID TEXT,VIEW_URL TEXT,DOWNLOAD_URL TEXT)')
-
-bot = telebot.TeleBot('5683443990:AAHvz0aQwW8ZI92KqIEnxw-6hh2jxy_6MDw') # creating a instance
-dbconnect = sqlite3.connect(r'c:\Users\Ilya Polonsky\Desktop\users9.db3')
+api=os.environ.get('API')
+img_path=('/appdata/img')
+bot = telebot.TeleBot(api)
+sql_path=('/appdata/db/db_data/bot_sql.db3')
+logger.info(f'{__name__} Bot instance created')
+dbconnect = sqlite3.connect(sql_path,check_same_thread=False)
+logger.info(f'{__name__} Database connected')
 pointer = dbconnect.cursor()
-pointer.execute('CREATE TABLE IF NOT EXISTS user_bot (USER_BOT_ID TEXT,VIEW_URL TEXT,DOWNLOAD_URL TEXT,PHOTO_ROOT TEXT, SEARCH_PHRASE TEXT)')
+pointer.execute('CREATE TABLE IF NOT EXISTS user_bot (USER_BOT_ID TEXT,VIEW_URL TEXT,DOWNLOAD_URL TEXT'
+                ',PHOTO_ROOT TEXT, SEARCH_PHRASE TEXT)')
+pointer.execute('CREATE TABLE IF NOT EXISTS user_data (USER_BOT_ID TEXT,REPLY TEXT)')
+
+
+def user_reply_check(user_id):
+    logger.info(f"{__name__} Check Reply or not User's preference in user_data table")
+    result = pointer.execute('''SELECT REPLY FROM user_data WHERE USER_BOT_ID=?''', (str(user_id),)).fetchone()
+    if not result:
+        pointer.execute('''INSERT INTO user_data (USER_BOT_ID,REPLY) VALUES(?,?)''', (str(user_id), 'true',))
+        dbconnect.commit()
+        return True
+    else:
+        if result[0] == '1':
+            logger.info(f"{__name__} User's prefernce is reply")
+            return True
+        else:
+            logger.info(f"{__name__} User's prefernce is not reply")
+            return False
 
 
 def database_check(text_to_search,user_id,photo_root=''):
-    sql_query = (f'''SELECT * WHERE SEARCH_PHRASE = {text_to_search}''')
-    result = pointer.execute(sql_query).fetchone()
+    logger.info(f"{__name__} Check id search phrase exists in database")
+    sql_query = (f'''SELECT DOWNLOAD_URL FROM user_bot WHERE SEARCH_PHRASE=?;''')
+    result = pointer.execute(sql_query, (text_to_search,)).fetchone()
     if result:
-        return result[0][3]
+        return result[0]
     else:
-        sql_query = (f'''INSERT INTO user_bot (USER_BOT_ID,VIEW_URL,DOWNLOAD_URL,PHOTO_ROOT, SEARCH_PHRASE) VALUES(?,?,?,?,?)''')
-        links = utils.search_download_youtube_video()
-        data = (user_id,links['youtube_url'], links['download_url'],photo_root,text_to_search)
-        pointer.execute(sql_query,data)
+        logger.info(f"{__name__} Search phrase doesn't exist in database, the data inserted")
+        sql_query = (f'''INSERT INTO user_bot 
+        (USER_BOT_ID,VIEW_URL,DOWNLOAD_URL,PHOTO_ROOT, SEARCH_PHRASE) VALUES(?,?,?,?,?)''')
+        links = utils.search_download_youtube_video(text_to_search)
+        logger.info(f"{__name__} Recieved data from youtube")
+        data = (user_id,links['youtube_url'], links['download_url'], photo_root, text_to_search)
+        pointer.execute(sql_query, data)
+        dbconnect.commit()
         return links['download_url']
 
 
-@bot.message_handler(content_types=['audio', 'document','sticker','video','video_note','voice', 'location', 'contact', 'new_chat_members', 'left_chat_member',
-'new_chat_title', 'new_chat_photo', 'delete_chat_photo', 'group_chat_created', 'supergroup_chat_created', 'channel_chat_created',
-'migrate_to_chat_id', 'migrate_from_chat_id', 'pinned_message', 'web_app_data'])
+@bot.message_handler(commands=['start', 'help', 'START', 'HELP'])
+def help_start_commands(message):
+    start_time = datetime.datetime.now()
+    logger.info(f"{__name__} Response to help/start command")
+    bot.send_message(chat_id=message.chat.id,text=f'Hello {message.from_user.username}\nThe supported inputs are:'
+                                                  f' Text and Photos.\nYou type a text and will recieve a link\n'
+                     f'to a video from youtube that you can\neasily to save on your device.\nI will only send you a HQ '
+                                                  f'videos, otherwise you shall recieve\n'
+                                                  f'No video with a decent quality available\n'
+                                                  f'\nOnce you send a photo it will be downloaded and saved.\n'
+                     f'For help use /help command.\n'
+                     f'By default all the message from me are in reply mode\nHowever, If you wish\n'
+                     f'to have message from me to be in not reply mode,'
+                                                  f' use /noqoute command\nand /qoute command\nreturn the default.'
+                                                  f'\nIt took:{datetime.datetime.now() - start_time}')
+
+
+@bot.message_handler(commands=['noqoute', 'NOQOUTE'])
+def noqoute_on(message):
+    start_time = datetime.datetime.now()
+    logger.info(f"{__name__} NoReply set")
+    pointer.execute('''UPDATE user_data SET REPLY=false WHERE USER_BOT_ID=?''',(str(message.from_user.id),))
+    dbconnect.commit()
+    bot.send_message(chat_id=message.chat.id,text=f'Dear {message.from_user.username}\n'
+                         f'Qoute Mode is OFF\nIt took:{datetime.datetime.now() - start_time}')
+
+
+@bot.message_handler(commands=['qoute', 'QOUTE'])
+def qoute_on(message):
+    start_time = datetime.datetime.now()
+    logger.info(f"{__name__} Replay set")
+    pointer.execute('''UPDATE user_data SET REPLY=true WHERE USER_BOT_ID=?''', (str(message.from_user.id),))
+    dbconnect.commit()
+    bot.reply_to(message,f'Dear {message.from_user.username}\n'
+                         f'Qoute Mode is ON\nIt took:{datetime.datetime.now() - start_time}')
+
+
+@bot.message_handler(content_types=['audio', 'document','sticker','video','video_note','voice', 'location', 'contact',
+                                    'new_chat_members', 'left_chat_member', 'new_chat_title', 'new_chat_photo',
+                                    'delete_chat_photo', 'group_chat_created', 'supergroup_chat_created',
+                                    'channel_chat_created', 'migrate_to_chat_id', 'migrate_from_chat_id',
+                                    'pinned_message', 'web_app_data'])
 def not_relevant(message):
-    bot.reply_to(message,'Sorry, but at this point of time\nthis type of enterence is not supported.')
+    start_time = datetime.datetime.now()
+    logger.info(f"{__name__} Response to all the content types except text or photo")
+    if user_reply_check(message.from_user.id):
+        bot.reply_to(message,'Sorry, but at this point of time\nthis type of enterence is not supported.')
+    else:
+        bot.send_message(chat_id=message.chat.id, text=f'Sorry, but at this point of time\nthis typ'
+                                                       f'e of enterence is not supported.\n'
+                                                       f'It took:{datetime.datetime.now() - start_time}')
 
 
 @bot.message_handler(content_types=['photo'])
-def down_(message):
-    bot.reply_to(message, 'Not Relevant')
+def download_photo(message):
+    start_time = datetime.datetime.now()
+    fileID = message.photo[-1].file_id
+    file_info = bot.get_file(fileID)
+    downloaded_file = bot.download_file(file_info.file_path)
+    img=f'{img_path}/{message.from_user.id}'
+    if not os.path.exists(img):
+        os.makedirs(img)
+    file_name = f'{message.from_user.id}_{str(uuid.uuid4())}.jpg'
+    with open(r"{path}/{filename}.jpg".format(path=img,filename=f'{file_name}'
+            ,user_id=message.from_user.id), 'wb') as new_file:
+        new_file.write(downloaded_file)
+    logger.info(f"{__name__} Photo downloaded to {img} as {file_name}")
+    if user_reply_check(message.from_user.id):
+        bot.reply_to(message, f'Your photo:\nfileId:{fileID}\nFile Info:{file_info}\n'
+                              f'saved with an Unique name in jpg format\nIt took'
+                              f':{datetime.datetime.now() - start_time}')
+    else:
+        bot.send_message(chat_id=message.chat.id,
+                         text=f'Your photo:\nfileId:{fileID}\nFile Info:{file_info}\n'
+                              f'saved with an Unique name in jpg format\nIt took'
+                              f':{datetime.datetime.now() - start_time}')
+
 
 @bot.message_handler(content_types=['text'])
 def text_message(message):
-    clean_text = message.text
-    bot.reply_to(message, database_check(clean_text))
+    start_time = datetime.datetime.now()
+    logger.info(f"{__name__} Main text content type handle executing")
+    if user_reply_check(message.from_user.id):
+        bot.reply_to(message, f'{database_check(message.text,message.from_user.id)}\n'
+                              f'It took: {datetime.datetime.now() - start_time}')
+    else:
+        bot.send_message(chat_id=message.chat.id,text=f'{database_check(message.text,message.from_user.id)}\n'
+                                                      f'It took: {datetime.datetime.now() - start_time}')
 
 
 def main():
-    bot.polling() # looking for message
+    bot.polling()
 
 
 if __name__ == '__main__':
--- utils.py
+++ utils.py
@@ -1,47 +1,11 @@
-import time
 from yt_dlp import YoutubeDL
-import mysql.connector
-from os import environ
-'''mydb = mysql.connector.connect(
-    host=environ.get('MYSQL_IP'),
-    user=environ.get('MYSQL_ROOT_USER'),
-    password=environ.get('MYSQL_ROOT_PASSWORD'))'''
+from loguru import logger
 
 
 def search_download_youtube_video(video_name, num_results=1,user_id=0,download=False):
-    """
-    This function downloads the first num_results search results from Youtube
-    :param video_name: string of the video name
-    :param num_results: integer representing how many videos to download
-    :return: list of paths to your downloaded video files
-    """
-    results = []
     with YoutubeDL() as ydl:
+        logger.info(f"{__name__}YoutubeDL from Util initiated")
         videos = ydl.extract_info(f"ytsearch{1}:{video_name}", download=False)['entries']
-        print(videos[0], 'type', type(videos[0]))
         for format in videos[0]['formats']:
             if format['format_id'] == '22' or format['format_id'] == '18':
-                print(f'Download Video:\n{format["url"]}')
-                return {'download':format["url"],'youtube_url':videos[0]['webpage_url']}
-                    #mycursor = mydb.cursor()
-'''                sql_query = "INSERT INTO users (user_bot_id,search_phrase,link_view,link_download) " \
-                            "VALUES (%s, %s, %s, %s)"
-                    values = (user_id,video_name,videos[0]['webpage_url'],format["url"])
-                    mycursor.execute(sql,val)
-                    mydb.commit()
-                    #mycursor.rowcount
-                data_dict = {'title':videos[0]['title'],'view':videos[0]['webpage_url'],'download':format["url"]}'''
-'''    
-        for video in videos:
-            print(f'VIEW:\n{video["webpage_url"]}\nDownload:\n{video}')
-            return
-            results.append({
-                'filename': ydl.prepare_filename(video),
-                'video_id': video['id'],
-                'title': video['title'],
-                'url': video['webpage_url']
-            })
-            print(f'VIEW:\n{video["webpage_url"]}\nDownload:\n{video["formats"][20]}')
-        '''
-
-#search_download_youtube_video('hello')
+                return {'download_url':format["url"],'youtube_url':videos[0]['webpage_url']}
